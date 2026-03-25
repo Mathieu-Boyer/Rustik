6c0af909ea4c05dc149dac4356a7b143264cbe21
mathieu.boyer <mathieu.boyer@epsa.com>
2026-03-25 12:40:50 +0100

    Inspection and fix of moves rotation direction

--- rustik/src/cube/debug.rs
+++ rustik/src/cube/debug.rs
@@ -23,6 +23,8 @@ impl Cube{
         })
     }
     pub fn show_slots(&self) {
+
+        println!("------ EDGES ------");
         for i in 0..12{
             let mask : i64 = 0b11111;
             let chunk =  (self.edges >> (i * 5)) & mask ;
@@ -31,21 +33,10 @@ impl Cube{
 
             let color = if slot == i { "\x1b[32m"} else {"\x1b[31m"};
 
-            print!("{}| {}{}[{}] \x1b[0m|",i,color, self.slot_from_int(i) , self.slot_from_int(slot));
+            print!("{} | {}{}[{}] \x1b[0m|",i,color, self.slot_from_int(i) , self.slot_from_int(slot));
 
 
             print!(" FlipState : {} |\n", is_flipped);
         }
     }
 }
-
-
-// pub fn switch_slots(&mut self, slot_id1 : i64, slot_id2 : i64) {
-//     let mask : i64 = 0b11111;
-//     let combined_masks : i64 = (mask << (5 * slot_id1)) | (mask << (5 * slot_id2));
-//     let chunk_at_slot1 : i64 = (self.edges >> (slot_id1 * 5)) & mask;
-//     let chunk_at_slot2 : i64 = (self.edges >> (slot_id2 * 5)) & mask;
-//
-//     self.edges &= !combined_masks;
-//     self.edges |= (chunk_at_slot2 << (5 * slot_id1)) | (chunk_at_slot1 << (5 * slot_id2)) ;
-// }
--- rustik/src/main.rs
+++ rustik/src/main.rs
@@ -10,12 +10,10 @@ fn main() {
 
     cube.display();
 
-    cube.apply_move_on_edges(0, false);
-    cube.apply_move_on_edges(5, false);
+    cube.apply_move_on_edges(1, true);
+    // cube.apply_move_on_edges(5, false);
     // cube.apply_move_on_edges(0, false);
     // cube.apply_move_on_edges(0, false);
 
      cube.show_slots();
-
-    
 }
