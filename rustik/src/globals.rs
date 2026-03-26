c482e5ecda65abd188083d5f2a5fbd1bb61cd978
mathieu.boyer <mathieu.boyer@epsa.com>
2026-03-26 13:47:50 +0100

    Cube now has heuristics

--- rustik/src/globals.rs
+++ rustik/src/globals.rs
@@ -1,27 +1,54 @@
- pub const AFFECTED_EDGE_SLOTS : [[i64; 4]; 6] = [
-     [0, 3, 2 , 1],    // U
-     [8, 9, 10, 11],  // D
-     [0, 4, 8, 7],    // F
-     [2, 6, 10, 5],   // B
-     [1, 5, 9, 4],    // R
-     [3, 7, 11, 6],   // L
+
+
+pub struct Move {
+    pub face : usize,
+    pub is_clockwise : bool
+}
+
+pub const MOVE_LIST : [Move; 12] = [
+    Move {face : 0, is_clockwise : true},
+    Move {face : 0, is_clockwise : false},
+
+    Move {face : 1, is_clockwise : true},
+    Move {face : 1, is_clockwise : false},
+
+    Move {face : 2, is_clockwise : true},
+    Move {face : 2, is_clockwise : false},
+
+    Move {face : 3, is_clockwise : true},
+    Move {face : 3, is_clockwise : false},
+
+    Move {face : 4, is_clockwise : true},
+    Move {face : 4, is_clockwise : false},
+
+    Move {face : 5, is_clockwise : true},
+    Move {face : 5, is_clockwise : false},
+];
+
+pub const AFFECTED_EDGE_SLOTS : [[i64; 4]; 6] = [
+ [0, 3, 2 , 1],    // U
+ [8, 9, 10, 11],  // D
+ [0, 4, 8, 7],    // F
+ [2, 6, 10, 5],   // B
+ [1, 5, 9, 4],    // R
+ [3, 7, 11, 6],   // L
 ];
 
 
- pub const AFFECTED_CORNERS_SLOTS : [[i64; 4]; 6] = [
-      [3, 2, 1, 0],    // U
-      [7, 4 , 5 , 6],  // D
-      [3, 0 , 4 , 7],  // F
-      [1, 2 , 6, 5],   // B
-      [0, 1, 5 , 4],   // R
-      [3, 7 , 6 , 2],  // L
- ];
+pub const AFFECTED_CORNERS_SLOTS : [[i64; 4]; 6] = [
+  [3, 2, 1, 0],    // U
+  [7, 4 , 5 , 6],  // D
+  [3, 0 , 4 , 7],  // F
+  [1, 2 , 6, 5],   // B
+  [0, 1, 5 , 4],   // R
+  [3, 7 , 6 , 2],  // L
+];
 
 
- pub const FACES : [usize; 6] = [
-     0, 1, 2, 3, 4, 5
- ];
+pub const FACES : [usize; 6] = [
+ 0, 1, 2, 3, 4, 5
+];
 
- pub const OPPOSITE_FACE : [usize; 6] = [
-      1, 0, 3, 2, 5, 4
- ];
+pub const OPPOSITE_FACE : [usize; 6] = [
+  1, 0, 3, 2, 5, 4
+];
--- rustik/src/solver/main_loop.rs
+++ rustik/src/solver/main_loop.rs
@@ -1,13 +1,13 @@
 use crate::cube::core::Cube;
+use crate::globals::MOVE_LIST;
 use crate::solver::core::Solver;
 
 impl Solver {
 
-    fn main_loop (&mut self, cube : &mut Cube) {
-
+    fn search (&self, cube : &mut Cube) {
     }
-    fn ida_star() {
-        
+    fn ida_star(&self, cube : &mut Cube) {
+
     }
     pub fn solve(&mut self, cube : &mut Cube) {
         cube.apply_move(0, true);
